b70a36e7cde3561e2b3d7c34c141f6b527422b17
TirthAgrawal12 <tirthagrawal12@gmail.com>
2024-10-25 17:01:41 +0530

    project initialized

---
+++ src/main/java/com/example/User/Profile/Management/UserProfileManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.User.Profile.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProfileManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserProfileManagementApplication.class, args);
+	}
+
+}
